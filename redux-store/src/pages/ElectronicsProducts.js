e134ebcb1f4775fc5ada40e39a5d9676f850328e
Kubastrzt <kubastrzt@gmail.com>
2022-07-25 14:28:45 +0200

    Cleaning code

--- redux-store/src/pages/ElectronicsProducts.js
+++ redux-store/src/pages/ElectronicsProducts.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Filter from "../components/Filter";
 import Footer from "../components/Footer";
-import {Link, useNavigate} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 
 const ElectronicsProducts = ()=>{
     const [switchButton, setSwitchButton] = React.useState(false)
